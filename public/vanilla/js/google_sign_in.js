afed23efba75360866a9a220dfbec4cdd64f3380
Jinyao Yuan <yjyao1112@gmail.com>
2017-03-04 02:27:01 -0800

    add log in function (no error handling)

--- public/vanilla/js/google_sign_in.js
+++ public/vanilla/js/google_sign_in.js
@@ -9,5 +9,20 @@
     messagingSenderId: "762957983152"
   };
   firebase.initializeApp(config);
+
+  // Get elements
+  const _email = document.getElementById('usr_email');
+  const _password = document.getElementById('usr_pwd');
+  const _loginBtn = document.getElementById('login_btn');
+  const _signupBtn = document.getElementById('signup_btn');
+
+  // when loginBtn is clicked, collect user info
+  _loginBtn.addEventListener('click', e => {
+      const email = _email.value;
+      const password = _password.value;
+      const auth = firebase.auth();
+      const promise = auth.signInWithEmailAndPassword(email, password);
+      promise.catch(e => console.log(e.message));
+  })
 }());
 
