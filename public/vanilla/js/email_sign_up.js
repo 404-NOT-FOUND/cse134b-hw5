b0076fa55b4ff8c2d1643b7778375b192354519e
Faye Tunggal <faye.tunggal@gmail.com>
2017-03-04 15:29:45 -0800

    sign out button & validation

--- public/vanilla/js/email_sign_up.js
+++ public/vanilla/js/email_sign_up.js
@@ -1,30 +1,38 @@
 
 (function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDgNzcJS-zqj-I3yUUXJp7EXp-6Vd6XsOo",
-    authDomain: "boardex-b8a9e.firebaseapp.com",
-    databaseURL: "https://boardex-b8a9e.firebaseio.com",
-    storageBucket: "boardex-b8a9e.appspot.com",
-    messagingSenderId: "762957983152"
-  };
-  firebase.initializeApp(config);
+    var config = {
+        apiKey: "AIzaSyDgNzcJS-zqj-I3yUUXJp7EXp-6Vd6XsOo",
+        authDomain: "boardex-b8a9e.firebaseapp.com",
+        databaseURL: "https://boardex-b8a9e.firebaseio.com",
+        storageBucket: "boardex-b8a9e.appspot.com",
+        messagingSenderId: "762957983152"
+    };
+    firebase.initializeApp(config);
 
-  const auth = firebase.auth();
+    const auth = firebase.auth();
 
-  const _email = document.getElementById("usr_email");
-  const _password = document.getElementById("usr_password");
-  const _passwordCheck = document.getElementById("usr_password_check");
-  const _signupBtn = document.getElementById("signup_btn");
+    const _email = document.getElementById("usr_email");
+    const _password = document.getElementById("usr_password");
+    const _passwordCheck = document.getElementById("usr_password_check");
+    const _signupBtn = document.getElementById("signup_btn");
 
-  _signupBtn.addEventListener('click', e => {
-      const email = _email.value;
-      const password = _password.value;
-      // TODO email check
-      // TODO password check
-      const promise = auth.createUserWithEmailAndPassword(email, password);
-      promise.catch(e => console.log(e.message));
-  });
+    _signupBtn.addEventListener('click', e => {
+        const email = _email.value;
+        const password = _password.value;
+        const passwordCheck = _passwordCheck.value;
+
+        const promise = auth.createUserWithEmailAndPassword(email, password);
+        promise.catch(e => console.log(e.message));
+
+        promise.then(onFulfilled, onRejected);
+
+        function onFulfilled(value) {
+            document.location.href = 'index.html';
+        }
+        function onRejected(error) {
+            alert(error.message);
+        }
+    });
 
 }());
 
--- public/vanilla/js/user_state.js
+++ public/vanilla/js/user_state.js
@@ -1,12 +1,42 @@
 
 (function() {
+    var config = {
+        apiKey: "AIzaSyDgNzcJS-zqj-I3yUUXJp7EXp-6Vd6XsOo",
+        authDomain: "boardex-b8a9e.firebaseapp.com",
+        databaseURL: "https://boardex-b8a9e.firebaseio.com",
+        storageBucket: "boardex-b8a9e.appspot.com",
+        messagingSenderId: "762957983152"
+    };
+
+    firebase.initializeApp(config);
+
     const auth = firebase.auth();
+
+    const _signupBtn = document.getElementById('signup');
+    const _signinBtn = document.getElementById('signin');
+    const _signoutBtn = document.getElementById('signout');
+
+    _signoutBtn.addEventListener('click', e => {
+        auth.signOut();
+
+        alert("You have succesfully signed out.");
+    })
 
     auth.onAuthStateChanged(user => {
         if (user) {
             console.log(user);
+            console.log('logged in');
+
+            _signupBtn.classList.add('hide');
+            _signinBtn.classList.add('hide');
+            _signoutBtn.classList.remove('hide');
+
         } else {
             console.log('not logged in');
+            
+            _signupBtn.classList.remove('hide');
+            _signinBtn.classList.remove('hide');
+            _signoutBtn.classList.add('hide');
         }
     });
 }());
