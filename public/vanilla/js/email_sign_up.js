1490a156620f5f06c4e41ab515ee7563d776b15b
Jinyao Yuan <yjyao1112@gmail.com>
2017-03-04 02:46:19 -0800

    add basic sign up functionality

--- public/vanilla/js/email_sign_up.js
+++ public/vanilla/js/email_sign_up.js
@@ -9,5 +9,20 @@
     messagingSenderId: "762957983152"
   };
   firebase.initializeApp(config);
+
+  const _email = document.getElementById("usr_email");
+  const _password = document.getElementById("usr_password");
+  const _passwordCheck = document.getElementById("usr_password_check");
+  const _signupBtn = document.getElementById("signup_btn");
+
+  _signupBtn.addEventListener('click', e => {
+      email = _email.value;
+      password = _password.value;
+      // TODO email check
+      // TODO password check
+      auth = firebase.auth();
+      const promise = auth.createUserWithEmailAndPassword(email, password);
+      promise.catch(e => console.log(e.message));
+  });
 }());
 
