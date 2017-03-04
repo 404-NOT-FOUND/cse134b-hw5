2e72a5e4d14a7ce0a2eb3cbe4e748ee82e506e43
Jinyao Yuan <yjyao1112@gmail.com>
2017-03-04 02:48:40 -0800

    minor change

--- public/vanilla/js/email_sign_up.js
+++ public/vanilla/js/email_sign_up.js
@@ -10,19 +10,21 @@
   };
   firebase.initializeApp(config);
 
+  const auth = firebase.auth();
+
   const _email = document.getElementById("usr_email");
   const _password = document.getElementById("usr_password");
   const _passwordCheck = document.getElementById("usr_password_check");
   const _signupBtn = document.getElementById("signup_btn");
 
   _signupBtn.addEventListener('click', e => {
-      email = _email.value;
-      password = _password.value;
+      const email = _email.value;
+      const password = _password.value;
       // TODO email check
       // TODO password check
-      auth = firebase.auth();
       const promise = auth.createUserWithEmailAndPassword(email, password);
       promise.catch(e => console.log(e.message));
   });
+
 }());
 
