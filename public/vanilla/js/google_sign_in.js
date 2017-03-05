c230096bb3f6bc0af571e0ca4dac6cfb922eba90
Faye Tunggal <faye.tunggal@gmail.com>
2017-03-04 16:12:52 -0800

    email & google signin

--- public/vanilla/js/google_sign_in.js
+++ public/vanilla/js/google_sign_in.js
@@ -1,28 +1,37 @@
 
 (function() {
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyDgNzcJS-zqj-I3yUUXJp7EXp-6Vd6XsOo",
-    authDomain: "boardex-b8a9e.firebaseapp.com",
-    databaseURL: "https://boardex-b8a9e.firebaseio.com",
-    storageBucket: "boardex-b8a9e.appspot.com",
-    messagingSenderId: "762957983152"
-  };
-  firebase.initializeApp(config);
+    // Initialize Firebase
 
-  // Get elements
-  const _email = document.getElementById('usr_email');
-  const _password = document.getElementById('usr_pwd');
-  const _loginBtn = document.getElementById('login_btn');
-  const _signupBtn = document.getElementById('signup_btn');
 
-  // when loginBtn is clicked, collect user info
-  _loginBtn.addEventListener('click', e => {
-      const email = _email.value;
-      const password = _password.value;
-      const auth = firebase.auth();
-      const promise = auth.signInWithEmailAndPassword(email, password);
-      promise.catch(e => console.log(e.message));
-  })
+    const auth = firebase.auth();
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    // Get elements
+    const _email = document.getElementById('usr_email');
+    const _password = document.getElementById('usr_pwd');
+    const _loginBtn = document.getElementById('google_login_btn');
+
+    // when loginBtn is clicked, collect user info
+    _loginBtn.addEventListener('click', e => {
+        firebase.auth().signInWithRedirect(provider);
+
+        firebase.auth().getRedirectResult().then(function(result) {
+            if (result.credential) {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = result.credential.accessToken;
+            }
+            // The signed-in user info.
+            var user = result.user;
+        }).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+    })
 }());
 
