18f9fc2c83c077348b1e032f6e786e9808682478
Faye Tunggal <faye.tunggal@gmail.com>
2017-03-06 11:43:13 -0800

    alert on success/error add/update game & redirect

--- public/vanilla/js/edit_game.js
+++ public/vanilla/js/edit_game.js
@@ -95,7 +95,12 @@ auth.onAuthStateChanged(user => {
                         'playerMax':  vm.game.playerMax,
                         'age':        vm.game.age,
                         'uid':        auth.currentUser.uid,
-                    }).then(console.log('pushed!'));
+                    }).then(function(snapshot) {
+                        alert('Game succesfully added.');
+                        document.location.href = 'info.html?t=' + vm.game.title;
+                    }, function(error) {
+                        alert('An error occured when trying to add your game.');
+                    });//console.log('pushed!'));
                 };
                 this.uploadImageWithCallBack(requireImg, add);
             },
@@ -108,7 +113,17 @@ auth.onAuthStateChanged(user => {
                         'playerMin':  vm.game.playerMin,
                         'playerMax':  vm.game.playerMax,
                         'age':        vm.game.age,
-                    }).then(console.log('updated'));
+                    }).then(function(snapshot) {
+                        alert('Game succesfully updated.');
+                        document.location.href = 'info.html?t=' + vm.game.title;
+                    }, function(error) {
+                        if(error.code === 'PERMISSION_DENIED') {
+                            alert('Error: You do not have permission to edit this game. Redirecting to the home page.');
+                            document.location.href = 'index.html';
+                        } else {
+                            alert('Error: ' + error.message);
+                        }
+                    });
                 };
                 this.uploadImageWithCallBack(update, update);
             },
