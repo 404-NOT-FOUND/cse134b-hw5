527e2f493b167b183af8c60820fc421b74564483
Faye Tunggal <faye.tunggal@gmail.com>
2017-03-05 17:42:22 -0800

    login redirect

--- public/vanilla/js/email_sign_in.js
+++ public/vanilla/js/email_sign_in.js
@@ -16,6 +16,7 @@
     const _email = document.getElementById('usr_email');
     const _password = document.getElementById('usr_pwd');
     const _loginBtn = document.getElementById('login_btn');
+    const _signupBtn = document.getElementById('signup_btn')
 
     // when loginBtn is clicked, collect user info
     _loginBtn.addEventListener('click', e => {
@@ -33,6 +34,10 @@
         function onRejected(error) {
             alert(error.message);
         }
-    })
+    });
+
+    _signupBtn.addEventListener('click', e => {
+        document.location.href = 'signup.html' + window.location.search;
+    });
 }());
 
