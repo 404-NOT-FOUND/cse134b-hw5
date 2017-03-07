f196a9473f7ce6370717c49a2647bdbb99258eaa
Jy <yjyao1112@gmail.com>
2017-03-06 19:20:06 -0800

    style check email_sign_up.js

--- public/vanilla/js/email_sign_up.js
+++ public/vanilla/js/email_sign_up.js
@@ -1,51 +1,45 @@
 
 (function() {
-    const auth = firebase.auth();
 
-    const _email = document.getElementById("usr_email");
-    const _password = document.getElementById("usr_password");
-    const _passwordCheck = document.getElementById("usr_password_check");
-    const _signupBtn = document.getElementById("signup_btn");
-    const _login = document.getElementById("login");
+const auth = firebase.auth();
 
-    console.log(window.location.search);
+const _email         = document.getElementById("usr_email");
+const _password      = document.getElementById("usr_password");
+const _passwordCheck = document.getElementById("usr_password_check");
+const _signupBtn     = document.getElementById("signup_btn");
+const _login         = document.getElementById("login");
 
-    _signupBtn.addEventListener('click', e => {
-        const email = _email.value;
-        const password = _password.value;
-        const passwordCheck = _passwordCheck.value;
+_signupBtn.addEventListener('click', e => {
+    const email         = _email.value;
+    const password      = _password.value;
+    const passwordCheck = _passwordCheck.value;
 
-        var matchPassword = false;
-        if(password === passwordCheck) {
-            matchPassword = true;
+    var isPasswordMatched = password === passwordCheck;
+    if(!isPasswordMatched) {
+        alert('Passwords do not match.');
+        return;
+    }
+
+    const promise = auth.createUserWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+    promise.then(onFulfilled, onRejected);
+
+    function onFulfilled(value) {
+
+        if(location.search == '?newgame'){
+            location.href = 'newgame.html';
         } else {
-            matchPassword = false;
-            alert('Passwords do not match.')
+            location.href = 'index.html';
         }
+    }
+    function onRejected(error) {
+        alert(error.message);
+    }
+});
 
-        if( matchPassword == true ) {
-            const promise = auth.createUserWithEmailAndPassword(email, password);
-            promise.catch(e => console.log(e.message));
-
-            promise.then(onFulfilled, onRejected);
-
-            function onFulfilled(value) {
-                if(window.location.search == '?newgame'){
-                    document.location.href = 'newgame.html';
-                } else {
-                    document.location.href = 'index.html';
-                }
-            }
-            function onRejected(error) {
-                alert(error.message);
-            }
-        }
-        
-    });
-
-    _login.addEventListener('click', e => {
-        document.location.href = 'login.html' + window.location.search;
-    });
+_login.addEventListener('click', e => {
+    location.href = 'login.html' + location.search;
+});
 
 }());
 
