660e4f311e49aeaf69ad33a80bd1c4f2eb841fd9
Jy <yjyao1112@gmail.com>
2017-03-06 22:16:36 -0800

    handle google sign in error code

--- public/vanilla/js/google_sign_in.js
+++ public/vanilla/js/google_sign_in.js
@@ -19,14 +19,8 @@ _loginBtn.addEventListener('click', e => {
             firebase.auth().signInWithRedirect(provider);
         }
     }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
         var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // TODO alert message?
+        alert(errorMessage);
     });
 });
 
