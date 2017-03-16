95c12e4ad3ccc57d2ebc4762ee60aaaaaf9043e7
Jy <yjyao1112@gmail.com>
2017-03-16 11:08:09 -0700

    apply filter results in the index page

--- public/js/read_list.js
+++ public/js/read_list.js
@@ -3,11 +3,55 @@
 Vue.use(VueFire);
 
 var gameDatabaseRef = database.ref('games');
+var taggingDatabaseRef = database.ref('tagging');
 
 var vm = new Vue({
-    el: "#board_list",
+    el: "#gamelist",
+    data: {
+        tags  : [],
+        titles: '',
+    },
     firebase: {
         games: gameDatabaseRef
+    },
+    methods: {
+        filterGame: function() {
+            vm.titles = '';
+            vm.games  = '';
+            console.log('tags: ' + JSON.stringify(vm.tags));
+            for (tag of vm.tags) {
+                var tagRef = taggingDatabaseRef.child('tags/'+tag);
+                tagRef.once('value', snap => {
+                    console.debug(snap.key);
+                    if (!snap.val()) { return; }
+                    if (vm.titles.length) {
+                        console.debug('titles and titles[tag]');
+                        console.debug(vm.titles);
+                        console.debug(Object.keys(snap.val()));
+                        vm.titles = this.intersect(vm.titles, Object.keys(snap.val()));
+                    } else {
+                        vm.titles = Object.keys(snap.val());
+                    }
+                    vm.updateGameList();
+                    console.log(vm.titles);
+                });
+            }
+        },
+        intersect: function(as, bs) {
+            if (as.length < 1 || bs.length < 1) { return []; }
+            return as.filter(elem => { return bs.indexOf(elem) >= 0; });
+        },
+        updateGameList: function() {
+            vm.games = [];
+            for (title of vm.titles) {
+                var gameRef = gameDatabaseRef.child(title);
+                gameRef.once('value', snap => {
+                    var game = snap.val();
+                    game['.key'] = snap.key;
+                    vm.games.push(game);
+                });
+            }
+        },
     },
 });
 
